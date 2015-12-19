8e0cf016f496b80c6140b011f05dd36ceee08433
Kristina <laiskai.kristina@gmail.com>
2015-12-19 15:35:32 +0200

    Scroll down function on click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+	// Back to top scroll effect
+	$("#back-to-top a[href^='#']").on('click', function(e) {
+	   e.preventDefault();
+	   var hash = this.hash;
+
+	   $('html, body').animate({
+	       scrollTop: $(this.hash).offset().top -50
+	     }, 700, function(){
+
+	       window.location.hash = hash;
+	     });
+	});
+
 	// Images Gallery
   	$('.image-link').magnificPopup({
   		type:'image',
